b455fced0fae0ecf1734510bbe5198c49f3417b9
leidy8864 <leidy.callupe@tecsup.edu.pe>
2020-12-22 18:30:56 -0500

    fix

--- src/views/Distributor.js
+++ src/views/Distributor.js
@@ -87,7 +87,7 @@ const Distributor = () => {
             <div className="container-grid distributor">
                 <div className="info-container">
                     <div className="title-blue-container" style={{ marginBottom: "40px" }}>
-                        {ubication.latitude ? <h3 style={{ width: "700px" }}>Se encontro un punto de venta cercano</h3> : <h3>Puntos de venta más cercano</h3>}
+                        {ubication.latitude ? <h3 style={{ width: "700px" }}>Se encontro un punto de venta cercano</h3> : <h3>Puntos de venta más cercanos</h3>}
                     </div>
                 </div>
             </div>
