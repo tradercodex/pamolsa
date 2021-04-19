80826e246be7a2bd165538b1c5c4165800630fd4
leidy8864 <leidy.callupe@tecsup.edu.pe>
2021-04-19 16:03:20 -0500

    update contact form

--- src/redux/actions/new.js
+++ src/redux/actions/new.js
@@ -15,8 +15,8 @@ import {
 
 import axios from 'axios'
 
-//const root = 'https://wspamolsa.com.pe/api'
-const root = 'https://ws.pamolsa.com.pe/api'
+const root = 'https://wspamolsa.com.pe/api'
+//const root = 'https://ws.pamolsa.com.pe/api'
 //const root = 'http://192.168.157.27/api'
 
 let token = localStorage.getItem('token')
