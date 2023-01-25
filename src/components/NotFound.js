d7b913c913ba4cb0b493147894cdfee2f42ed72e
leidy.callupesantisteban <leidy.callupesantisteban@tcs.com>
2023-01-25 18:59:53 -0500

    new catalog

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import Header from './Header'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
+    const { t } = useTranslation();
     return (
         <Fragment>
-            <Header />
+            <Header t={t} />
             <div className="no-quote">
                 <p>No se pudo encontrar la página solicitada, te aconsejamos regresar al inicio</p>
                 <Link className="quote-back" to="/">regresar</Link>
