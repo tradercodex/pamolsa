2e8ec93e5c9fd2044f2f9cc48ff001d0b47d6fab
Max <maxpalominomendoza@gmail.com>
2023-10-13 16:51:16 -0500

    validar politica

--- src/views/Claims.js
+++ src/views/Claims.js
@@ -1,75 +1,76 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux'
-import ClaimsForm from '../components/ClaimsForm';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import './../styles/claims.css'
-import { useTranslation } from 'react-i18next';
+import React, { Fragment, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import ClaimsForm from "../components/ClaimsForm";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "./../styles/claims.css";
+import { useTranslation } from "react-i18next";
 
 const Claims = () => {
+  const cart = useSelector((state) => state.cart);
 
-    const cart = useSelector(state => state.cart)
+  const [cartItems, setCartItems] = useState(cart.cartItems);
 
-    const [cartItems, setCartItems] = useState(cart.cartItems)
+  useEffect(() => {
+    const movilOpen = document.getElementById("movil");
+    const header = document.getElementById("header");
+    const movilClose = document.getElementById("close-movil");
 
-    useEffect(() => {
+    movilOpen.addEventListener("click", function () {
+      header.classList.add("movile-active");
+    });
 
-        const movilOpen = document.getElementById('movil');
-        const header = document.getElementById('header')
-        const movilClose = document.getElementById('close-movil')
+    movilClose.addEventListener("click", function () {
+      header.classList.remove("movile-active");
+    });
 
-        movilOpen.addEventListener('click', function () {
-            header.classList.add('movile-active')
-        })
+    setCartItems(cart.cartItems);
+  }, [cart.cartItems]);
 
-        movilClose.addEventListener('click', function () {
-            header.classList.remove('movile-active')
-        })
+  let number = Object.keys(cartItems).length;
+  const { t } = useTranslation();
 
-        setCartItems(cart.cartItems)
-    }, [cart.cartItems])
+  return (
+    <Fragment>
+      <Header number={number} t={t} />
 
-    let number = Object.keys(cartItems).length
-    const { t } = useTranslation();
-
-    return (
-        <Fragment>
-            <Header number={number} t={t} />
-
-            <div className="Claims">
-                <div className="content-claims">
-                    <div>
-                        <h2>Libro de reclamaciones</h2>
-                        <p className="subtitle-claims">Estimado cliente si requiere una atención inmediata, lo invitamos a usar nuestro canal de atención: (01) 641-9595.
-                            Gracias por ayudarnos a mejorar nuestro servicio.</p>
-                    </div>
-                    <div className="divider">
-                        <span></span>
-                    </div>
-                    <div className="services-claims">
-                        <div>
-                            <img src={require('../images/img/empresa.png')} alt="" />
-                            <p>Razón Social</p>
-                            <span>PERUANA DE MOLDEADOS S.A.C.</span>
-                        </div>
-                        <div>
-                            <img src={require('../images/img/ruc.png')} alt="" />
-                            <p>RUC</p>
-                            <span>20251995967</span>
-                        </div>
-                        <div>
-                            <img src={require('../images/img/direccion.png')} alt="" />
-                            <p>{t('contacto.direccion')}</p>
-                            <span>Av. Elmer Faucett 3486 Urb. Bocanegra, Callao</span>
-                        </div>
-                    </div>
-                </div>
+      <div className="Claims">
+        <div className="content-claims">
+          <div>
+            <h2>Libro de reclamaciones</h2>
+            <p className="subtitle-claims">
+              Estimado cliente si requiere una atención inmediata, lo invitamos
+              a usar nuestro canal de atención: (01) 641-9595. Gracias por
+              ayudarnos a mejorar nuestro servicio.
+            </p>
+          </div>
+          <div className="divider">
+            <span></span>
+          </div>
+          <div className="services-claims">
+            <div>
+              <img src={require("../images/img/empresa.png")} alt="" />
+              <p>Razón Social</p>
+              <span>PERUANA DE MOLDEADOS S.A.C.</span>
             </div>
+            <div>
+              <img src={require("../images/img/ruc.png")} alt="" />
+              <p>RUC</p>
+              <span>20251995967</span>
+            </div>
+            <div>
+              <img src={require("../images/img/direccion.png")} alt="" />
+              <p>{t("contacto.direccion")}</p>
+              <span>Av. Elmer Faucett 3486 Urb. Bocanegra, Callao</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <ClaimsForm t={t} />
-            <Footer t={t} />
-        </Fragment>
-    );
-}
+      <ClaimsForm t={t} />
+      <Footer t={t} />
+    </Fragment>
+  );
+};
 
 export default Claims;
